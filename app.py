976216198b73c7971a156c0a57d35aac1406a6df
gitmanluke <luke628@bu.edu>
2026-02-13 15:06:47 -0500

    feat:added support for player head models

--- app.py
+++ app.py
@@ -12,25 +12,43 @@ SERVER_ADDRESS = os.getenv('SERVER_ADDRESS', 'hypixel.net')  # default fallback
 app = FastAPI()
 current_data = {}
 
+async def request_heads(players):
+    heads = {}
+    for player in players:
+        uuid = player.get('uuid')
+        name = player.get('name')
+        if uuid and name:
+            head_url = f'https://mc-heads.net/avatar/{uuid}/24'
+            heads[name] = head_url
+    return heads
+
 
 async def fetch_data_loop():
     async with httpx.AsyncClient() as client:
         while True:
             response = await client.get(f'https://api.mcsrvstat.us/3/{SERVER_ADDRESS}')
-            stats_dict = json.loads(response.text)
-            reached = stats_dict['debug']['ping']
+            stats_dict = response.json()
+            reached = stats_dict.get('debug', {}).get('ping', False)
 
-            if (reached):
+            if (reached):   
                 print("SERVER REACHED")
-                online = stats_dict['online']
+                online = stats_dict.get('online', False)
                 if (online):
+                    players_data = stats_dict.get('players', {})
+                    players_list = players_data.get('list', [])
+
+                    heads = await request_heads(players_list)
+
+                    current_data['players'] = players_list
+                    current_data['heads'] = heads
                     current_data['online'] = True
-                    if (stats_dict['players']['list'] != None):
-                        current_data['players'] = stats_dict['players']['list']
+                else:
+                    current_data['online'] = False
+                    current_data['players'] = []
             else:
                 print("SERVER DOES NOT EXIST OR IS NOT ACCESSIBLE")
 
-            await asyncio.sleep(10)
+            await asyncio.sleep(10000)
 
 
 @app.on_event("startup")
@@ -49,4 +67,3 @@ async def home():
     # Serve your HTML page
     with open("templates/index.html") as f:
         return f.read()
-
